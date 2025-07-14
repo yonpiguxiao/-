396cf7b295f58bb04a5f399416a85ffaf1acac20
yonpiguxiao <1148419744@qq.com>
2025-07-14 16:49:49 +0800

    增加了全局异常处理

---
+++ seven-oj/oj-common/oj-common-security/src/main/java/com/seven/common/security/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.seven.common.security;
+import com.seven.common.core.domain.R;
+import com.seven.common.core.enums.ResultCode;
+import jakarta.servlet.http.HttpServletRequest;
+//import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+/**
+ * 全局异常处理器
+ */
+@RestControllerAdvice
+//@Slf4j
+public class GlobalExceptionHandler
+{
+    /**
+     * 请求⽅式不⽀持
+     */
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public R<?>
+    handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException e, HttpServletRequest request)
+    {
+        String requestURI = request.getRequestURI();
+// log.error("请求地址'{}',不⽀持'{}'请求", requestURI, e.getMethod());
+        return R.fail(ResultCode.ERROR);
+    }
+    /**
+     * 拦截运⾏时异常
+     */
+    @ExceptionHandler(RuntimeException.class)
+    public R<?> handleRuntimeException(RuntimeException e, HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+// log.error("请求地址'{}',发⽣异常.", requestURI, e);
+        return R.fail(ResultCode.ERROR);
+    }
+    /**
+     * 系统异常
+     */
+    @ExceptionHandler(Exception.class)
+    public R<?> handleException(Exception e, HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+// log.error("请求地址'{}',发⽣异常.", requestURI, e);
+        return R.fail(ResultCode.ERROR);
+    }
+}
