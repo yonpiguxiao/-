19cd6c5a26285aae4b23aac35c68dd1ec1b90411
yonpiguxiao <1148419744@qq.com>
2025-07-25 22:29:24 +0800

    题目列表数据库mysql表设计

--- seven-oj/deploy/init.sql
+++ seven-oj/deploy/init.sql
@@ -1,3 +1,5 @@
+
+--管理员表
 create table tb_sys_user(
 
 user_id bigint unsigned not null comment '用户id(主键)',
@@ -12,3 +14,37 @@ update_time datetime comment '更新时间',
 primary key (`user_id`),
 unique key `idx_user_account` (`user_account`)
 )
+
+
+
+--题库管理
+create table tb_question(
+question_id bigint unsigned not null comment '题目id',
+title varchar(50) not null comment '题目标题',
+difficulty tinyint not null comment '题目难度(1:简单  2:中等  3:困难)',
+time_limit int not null comment '时间限制:ms',
+space_limit int not null comment '空间限制:bite',
+content varchar(1000) not null comment '题目内容',
+question_case varchar(1000) comment '题目用例',
+default_code varchar(500) not null comment '默认代码块',
+main_func varchar(500) not null comment 'main函数',
+create_by bigint unsigned not null comment '创建人',
+create_time datetime not null comment '创建时间',
+update_by bigint unsigned comment '更新人',
+update_time datetime comment '更新时间',
+primary key(`question_id`)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 前端/oj-fe-b/src/views/Layout.vue
+++ 前端/oj-fe-b/src/views/Layout.vue
@@ -1,59 +1,93 @@
 <template>
     <el-container class="layout-container">
-            <el-header class="el-header">
-                <el-dropdown>
-                    <span class="el-dropdown__box">
-                        <div>
-                            <strong>当前⽤⼾：</strong>超级管理员
-                        </div>
-                        <el-icon>
-                            <ArrowDownBold />
-                        </el-icon>
-                    </span>
-                    <template #dropdown>
-                        <el-dropdown-menu>
-                            <el-dropdown-item :icon="SwitchButton">退出登录</el-dropdown-item>
-                        </el-dropdown-menu>
-                    </template>
-                </el-dropdown>
-            </el-header>
-            <el-main class="layout-bottom-box">
-                <div class="left">
-                    <el-aside width="200px" class="el-aside">
-                        <el-menu class="el-menu" router>
-                            <el-menu-item index="/oj/layout/cuser">
-                                <el-icon>
-                                    <Management />
-                                </el-icon>
-                                <span>⽤⼾管理</span>
-                            </el-menu-item>
-                            <el-menu-item index="/oj/layout/question">
-                                <el-icon>
-                                    <Management />
-                                </el-icon>
-                                <span>题⽬管理</span>
-                            </el-menu-item>
-                            <el-menu-item index="/oj/layout/exam">
-                                <el-icon>
-                                    <Management />
-                                </el-icon>
-                                <span>竞赛管理</span>
-                            </el-menu-item>
-                        </el-menu>
-                    </el-aside>
-                </div>
-                <div class="right">
-                    <RouterView />
-                </div>
-            </el-main>
+        <el-header class="el-header">
+            <el-dropdown>
+                <span class="el-dropdown__box">
+                    <div>
+                        <strong>当前⽤⼾：</strong>{{ loginUser.nickName }}
+                    </div>
+                    <el-icon>
+                        <ArrowDownBold />
+                    </el-icon>
+                </span>
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item @click="logout" :icon="SwitchButton">退出登录</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+        </el-header>
+        <el-main class="layout-bottom-box">
+            <div class="left">
+                <el-aside width="200px" class="el-aside">
+                    <el-menu class="el-menu" router>
+                        <el-menu-item index="/oj/layout/cuser">
+                            <el-icon>
+                                <Management />
+                            </el-icon>
+                            <span>⽤⼾管理</span>
+                        </el-menu-item>
+                        <el-menu-item index="/oj/layout/question">
+                            <el-icon>
+                                <Management />
+                            </el-icon>
+                            <span>题⽬管理</span>
+                        </el-menu-item>
+                        <el-menu-item index="/oj/layout/exam">
+                            <el-icon>
+                                <Management />
+                            </el-icon>
+                            <span>竞赛管理</span>
+                        </el-menu-item>
+                    </el-menu>
+                </el-aside>
+            </div>
+            <div class="right">
+                <RouterView />
+            </div>
+        </el-main>
     </el-container>
 </template>
 <script setup>
+import { 
+    getUserInfoService,
+    logoutService
+ } from '@/apis/suser';
 import {
     Management,
     ArrowDownBold,
     SwitchButton
 } from '@element-plus/icons-vue'
+import { ElMessageBox } from 'element-plus';
+import { reactive } from 'vue';
+import { removeToken } from '@/utils/cookie';
+import router from '@/router';
+const loginUser = reactive({
+    nickName: ''
+})
+
+async function getUserInfo() {
+    const userInfo = await getUserInfoService()
+    loginUser.nickName = userInfo.data.nickName
+}
+
+getUserInfo()
+
+async function logout() {
+    await ElMessageBox.confirm(
+        '确认退出',
+        '温馨提示',
+        {
+            confirmButtonText: '确认',
+            cancelButtonText: '退出',
+            type: 'warning',
+        }
+    )
+    await logoutService()
+    removeToken()
+    router.push("/oj/login")
+    //console.log('退出登录')
+}
 </script>
 <style lang="scss" scoped>
 .layout-container {
